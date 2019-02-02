616b976f40ebf6afcac4bfa92e39c6b1e847633f
Sergei Yarema <isara.isara8@gmail.com>
2019-02-02 23:27:13 +0200

    Add misc classes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 /*
 Вариант 6
 Предметная область  Агентство новостей
@@ -9,7 +10,59 @@
 
 #include <iostream>
 
+using namespace std;
 
+class Agency {
+public:
+    Agency(std::string _name) :
+            name(std::move(_name)) {}
+
+private:
+    std::string name;
+};
+
+class News {
+    News(std::string _title, std::string _text, int _Id) :
+            title(std::move(_title)), text(std::move(_text)), categoryId(_Id) {}
+
+private:
+    std::string title;
+    std::string text;
+    int categoryId;
+};
+
+class Category {
+    Category(const std::string &_name) : name(_name) {
+        id = MAX_ID;
+        MAX_ID++;
+    }
+
+    std::string getName() { return name; }
+
+private:
+    int id;
+    std::string name;
+
+    static int MAX_ID;
+};
+
+/// Hasher ///
+
+class Hash {
+    static int universalHash(int a, int b, int p, int m, int x) {
+        int hash = ((a * x + b) % p) % m;
+        return hash;
+    }
+};
+
+template<typename T>
+class HashTable {
+    HashTable<T>(const T[]);
+
+    bool exists(const T &val);
+
+
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
