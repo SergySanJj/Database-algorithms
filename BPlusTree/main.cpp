9c5012254640a0975ddd8a78137ceab06e8eba52
Sergei Yarema <isara.isara8@gmail.com>
2019-04-04 09:51:51 +0300

    Start BPlusTree

---
+++ BPlusTree/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+template <typename  KEY,typename DAT>
+class Node{
+public:
+
+    KEY key;
+    DAT data;
+private:
+};
+
+template <typename  KEY,typename DAT>
+class BPlusTree {
+public:
+
+private:
+    Node<KEY,DAT> *Root = nullptr;
+};
+
+
+
+int main() {
+
+    return 0;
+}
