2dcf32281625a0804dc13233b4f6364798dc5b9d
Sergei Yarema <isara.isara8@gmail.com>
2019-04-13 20:01:54 +0200

    Update Binomial Heap

--- BinomialHeap/main.cpp
+++ BinomialHeap/main.cpp
@@ -1,294 +1,238 @@
-#include <iostream>
 #include <vector>
-#include <list>
 #include <algorithm>
-#include <limits.h>
+#include <stdlib.h>
+#include <iostream>
 
-using namespace std;
 
-template<typename KEY, typename DAT>
-class Node {
-public:
-    Node(KEY _key, DAT _data) {
-        key = _key;
-        data = _data;
-        degree = 0;
-        child = parent = sibling = nullptr;
+template<typename T>
+struct BinomialNode {
+    T mValue;
+    BinomialNode *mRight;
+    BinomialNode *mChild;
+
+    BinomialNode(const T &value, BinomialNode *right, BinomialNode *child) {
+        mValue = value;
+        mRight = right;
+        mChild = child;
     }
-
-    KEY key;
-    DAT data;
-
-    Node<KEY, DAT> *child;
-    Node<KEY, DAT> *sibling;
-    Node<KEY, DAT> *parent;
-
-    int degree;
-private:
 };
 
-template<typename KEY, typename DAT>
+
+template<typename T>
 class BinomialHeap {
 public:
-    void insert(KEY key, DAT data) {
-        _heap = Insertion(_heap, key, data);
+    BinomialHeap();
+
+    ~BinomialHeap();
+
+    BinomialHeap(const BinomialHeap &);
+
+    BinomialHeap &operator=(const BinomialHeap &);
+
+    void push(const T &);
+
+    const T &top() const;
+
+    void pop();
+
+    void merge(BinomialHeap &other);
+
+    size_t size() const;
+
+    bool empty() const;
+
+    void swap(BinomialHeap &other);
+
+    void print(){
+        for (auto &root:mHeap){
+            std::cout << "( ";
+            print(root);
+            std::cout << " )";
+        }
     }
 
-    void print() {
-        ShowHeap(_heap);
-    }
-
-    pair<KEY, DAT> getMin() {
-        auto res = getMin(_heap);
-        return make_pair(res->key, res->data);
-    }
-
-    void extractMin() {
-        _heap = extractMin(_heap);
-    }
-
-    pair<KEY, DAT> find(KEY key) {
-        auto t = findNode(key);
-        if (t != nullptr)
-            return make_pair(t->key, t->data);
-        else
-            return make_pair(KEY(), DAT());
-    }
-
-    void decrease(KEY key, KEY newKey) {
-        auto t = findNode(key);
-        if (t != nullptr)
-            decreaseKey(_heap, t, newKey);
-        else
-            return;
-    }
-
-    template<typename K, typename D>
-    friend BinomialHeap<K, D> operator+(BinomialHeap<K, D> const &lhs, BinomialHeap<K, D> const &rhs);
 
 private:
-    list<Node<KEY, DAT> *> _heap;
+    std::vector<BinomialNode<T> *> mHeap;
+    size_t mSize;
 
-    Node<KEY, DAT> *Merge(Node<KEY, DAT> *tree1, Node<KEY, DAT> *tree2) {
-        if (tree1->key > tree2->key) {
-            swap(tree1->key, tree2->key);
-            swap(tree1->data, tree2->data);
-        }
-        tree2->parent = tree1;
-        tree2->sibling = tree1->child;
-        tree1->child = tree2;
-        tree1->degree++;
-        return tree1;
-    }
+    void print(BinomialNode<T> *root){
+        std::cout << root->mValue << " ";
 
-    static list<Node<KEY, DAT> *> Union(list<Node<KEY, DAT> *> heap1, list<Node<KEY, DAT> *> heap2) {
-        list<Node<KEY, DAT> *> newHeap;
-        auto i = heap1.begin();
-        auto j = heap2.begin();
-        while (i != heap1.end() && j != heap2.end()) {
-            if ((*i)->degree <= (*j)->degree) {
-                newHeap.push_back(*i);
-                i++;
-            } else {
-                newHeap.push_back(*j);
-                j++;
-            }
-        }
-        while (i != heap1.end()) {
-            newHeap.push_back(*i);
-            i++;
-        }
-        while (j != heap2.end()) {
-            newHeap.push_back(*j);
-            j++;
-        }
-        return newHeap;
-    }
-
-    // reorder degrees
-    list<Node<KEY, DAT> *> Order(list<Node<KEY, DAT> *> MyHeap) {
-        if (MyHeap.size() <= 1)
-            return MyHeap;
-        list<Node<KEY, DAT> *> newHeap;
-        auto i = MyHeap.begin();
-        auto j = MyHeap.begin();
-        auto k = MyHeap.begin();
-
-        if (MyHeap.size() == 2) {
-            j = i;
-            j++;
-            k = MyHeap.end();
-        } else {
-            j++;
-            k = j;
-            k++;
-        }
-        while (i != MyHeap.end()) {
-            if (j == MyHeap.end())
-                i++;
-            else if ((*i)->degree < (*j)->degree) {
-                i++;
-                j++;
-                if (k != MyHeap.end())
-                    k++;
-            } else if (k != MyHeap.end() && (*i)->degree == (*j)->degree && (*i)->degree == (*k)->degree) {
-                i++;
-                j++;
-                k++;
-            } else if ((*i)->degree == (*j)->degree) {
-                Node<KEY, DAT> *temp;
-                *i = Merge(*i, *j);
-                j = MyHeap.erase(j);
-                if (k != MyHeap.end())
-                    k++;
-            }
-        }
-        return MyHeap;
-    }
-
-    list<Node<KEY, DAT> *> InsertTree(list<Node<KEY, DAT> *> _heap, Node<KEY, DAT> *tree) {
-        list<Node<KEY, DAT> *> temp;
-        temp.push_back(tree);
-        temp = Union(_heap, temp);
-        return Order(temp);
-    }
-
-    list<Node<KEY, DAT> *> DeleteMin(Node<KEY, DAT> *tree) {
-        list<Node<KEY, DAT> *> heap;
-        auto *temp = tree->child;
-        Node<KEY, DAT> *minimum;
-        while (temp) {
-            minimum = temp;
-            temp = temp->sibling;
-            minimum->sibling = nullptr;
-            heap.push_front(minimum);
-        }
-        return heap;
-    }
-
-    list<Node<KEY, DAT> *> Insertion(list<Node<KEY, DAT> *> _head, KEY key, DAT data) {
-        auto *temp = new Node<KEY, DAT>(key, data);
-        return InsertTree(_head, temp);
-    }
-
-    Node<KEY, DAT> *getMin(list<Node<KEY, DAT> *> _heap) {
-        auto i = _heap.begin();
-        auto *temp = *i;
-        while (i != _heap.end()) {
-            if ((*i)->key < temp->key)
-                temp = *i;
-            i++;
-        }
-        return temp;
-    }
-
-    list<Node<KEY, DAT> *> extractMin(list<Node<KEY, DAT> *> _heap) {
-        list<Node<KEY, DAT> *> newHeap, lo;
-        Node<KEY, DAT> *temp;
-        temp = getMin(_heap);
-
-        for (auto &it:_heap) {
-            if (it != temp) {
-                newHeap.push_back(it);
-            }
-        }
-
-        lo = DeleteMin(temp);
-        newHeap = Union(newHeap, lo);
-        newHeap = Order(newHeap);
-        return newHeap;
-    }
-
-    void ShowTree(Node<KEY, DAT> *root) {
-        while (root) {
-            cout << "(" << root->key << ", " << root->data << ") ";
-            ShowTree(root->child);
-            root = root->sibling;
-        }
-    }
-
-
-    void ShowHeap(list<Node<KEY, DAT> *> heap) {
-        for (auto &el: heap)
-            ShowTree(el);
-    }
-
-    void decreaseKey(list<Node<KEY, DAT> *> heap, Node<KEY, DAT> *x, KEY k) {
-        if (k > x->key) {
-            return;
-        }
-        x->key = k;
-        auto z = x->parent;
-        while (z != nullptr) {
-            if (z->key < x->key) {
-                swap(z->key, x->key);
-                swap(z->data, x->data);
-                z = x->parent;
-            } else break;
-        }
-    }
-
-    Node<KEY, DAT> *findNode(KEY key) {
-        for (auto el:_heap) {
-            auto t = treeFind(key, el);
-            if (t != nullptr)
-                return t;
-        }
-        return nullptr;
-    }
-
-    Node<KEY, DAT> *treeFind(KEY key, Node<KEY, DAT> *root) {
-        if (root == nullptr)
-            return nullptr;
-
-        if (root->key == key)
-            return root;
-        if (root->key < key) {
-            auto t = treeFind(key, root->child);
-            if (t != nullptr)
-                return t;
-            t = treeFind(key, root->sibling);
-        }
-        return nullptr;
     }
 };
 
-template<typename KEY, typename DAT>
-BinomialHeap<KEY, DAT> operator+(BinomialHeap<KEY, DAT> const &lhs, BinomialHeap<KEY, DAT> const &rhs) {
-    auto tmp = lhs;
-    tmp._heap = BinomialHeap<KEY, DAT>::Union(tmp._heap, rhs._heap);
-    return tmp;
+template<typename T>
+bool CompareNodesByValue(const BinomialNode<T> *lhs,
+                         const BinomialNode<T> *rhs) {
+    if (!lhs || !rhs)
+        return !lhs < !rhs;
+    return lhs->mValue < rhs->mValue;
+}
+
+template<typename T>
+BinomialNode<T> *MergeTrees(BinomialNode<T> *lhs, BinomialNode<T> *rhs) {
+    if (rhs->mValue < lhs->mValue)
+        std::swap(lhs, rhs);
+    rhs->mRight = lhs->mChild;
+    lhs->mChild = rhs;
+    return lhs;
 }
 
 
+template<typename T>
+void BinomialHeapMerge(std::vector<BinomialNode<T> *> &lhs,
+                       std::vector<BinomialNode<T> *> &rhs) {
+    std::vector<BinomialNode<T> *> result;
+    const size_t maxOrder = std::max(lhs.size(), rhs.size());
+    lhs.resize(maxOrder);
+    rhs.resize(maxOrder);
+    BinomialNode<T> *carry = nullptr;
+
+    for (size_t order = 0; order < maxOrder; ++order) {
+        std::vector<BinomialNode<T> *> trees;
+        if (carry)
+            trees.push_back(carry);
+        if (lhs[order])
+            trees.push_back(lhs[order]);
+        if (rhs[order])
+            trees.push_back(rhs[order]);
+
+        if (trees.empty()) {
+            result.push_back(nullptr);
+            carry = nullptr;
+        } else if (trees.size() == 1) {
+            result.push_back(trees[0]);
+            carry = nullptr;
+        } else if (trees.size() == 2) {
+            result.push_back(nullptr);
+            carry = MergeTrees(trees[0], trees[1]);
+        } else {
+            result.push_back(trees[0]);
+            carry = MergeTrees(trees[1], trees[2]);
+        }
+    }
+
+    if (carry)
+        result.push_back(carry);
+    rhs.clear();
+    lhs = result;
+}
+
+
+template<typename T>
+void DestroyBinomialTree(BinomialNode<T> *root) {
+    if (!root) return;
+    DestroyBinomialTree(root->mRight);
+    DestroyBinomialTree(root->mChild);
+    delete root;
+}
+
+template<typename T>
+BinomialNode<T> *CloneBinomialTree(BinomialNode<T> *root) {
+    if (!root) return nullptr;
+    return new BinomialNode<T>(root->mValue,
+                               CloneBinomialTree(root->mRight),
+                               CloneBinomialTree(root->mChild));
+}
+
+template<typename T>
+BinomialHeap<T>::BinomialHeap() {
+    mSize = 0;
+}
+
+template<typename T>
+BinomialHeap<T>::~BinomialHeap() {
+    std::for_each(mHeap.begin(), mHeap.end(), DestroyBinomialTree<T>);
+}
+
+template<typename T>
+BinomialHeap<T>::BinomialHeap(const BinomialHeap &other) {
+    mSize = other.mSize;
+    for (size_t i = 0; i < mSize; ++i)
+        mHeap.push_back(CloneBinomialTree(other.mHeap[i]));
+}
+
+template<typename T>
+BinomialHeap<T> &BinomialHeap<T>::operator=(const BinomialHeap<T> &other) {
+    BinomialHeap copy(other);
+    swap(copy);
+    return *this;
+}
+
+template<typename T>
+void BinomialHeap<T>::swap(BinomialHeap &other) {
+    mHeap.swap(other.mHeap);
+    std::swap(mSize, other.mSize);
+}
+
+template<typename T>
+size_t BinomialHeap<T>::size() const {
+    return mSize;
+}
+
+template<typename T>
+bool BinomialHeap<T>::empty() const {
+    return size() == 0;
+}
+
+template<typename T>
+const T &BinomialHeap<T>::top() const {
+    return (*std::min_element(mHeap.begin(), mHeap.end(),
+                              CompareNodesByValue<T>))->mValue;
+}
+
+template<typename T>
+void BinomialHeap<T>::push(const T &value) {
+    std::vector<BinomialNode<T> *> singleton;
+    singleton.push_back(new BinomialNode<T>(value, nullptr, nullptr));
+    BinomialHeapMerge(mHeap, singleton);
+    mSize++;
+}
+
+template<typename T>
+void BinomialHeap<T>::merge(BinomialHeap &other) {
+    BinomialHeapMerge(mHeap, other.mHeap);
+    mSize += other.mSize;
+    other.mSize = 0;
+}
+
+template<typename T>
+void BinomialHeap<T>::pop() {
+
+    typename std::vector<BinomialNode<T> *>::iterator minElem =
+            std::min_element(mHeap.begin(), mHeap.end(),
+                             CompareNodesByValue<T>);
+    std::vector<BinomialNode<T> *> children;
+    for (BinomialNode<T> *child = (*minElem)->mChild;
+         child != nullptr; child = child->mRight)
+        children.push_back(child);
+
+    std::reverse(children.begin(), children.end());
+
+    for (size_t i = 0; i < children.size(); ++i)
+        children[i]->mRight = nullptr;
+    delete *minElem;
+    *minElem = nullptr;
+    if (minElem == mHeap.end() - 1)
+        mHeap.pop_back();
+    BinomialHeapMerge(mHeap, children);
+    mSize--;
+}
+
+#include <iostream>
+
 int main() {
-    BinomialHeap<int, int> bh;
-    bh.insert(1, 1);
-    bh.insert(3, 2);
-    bh.insert(-1, 3);
-    bh.insert(11, 4);
+    BinomialHeap<int> bh;
+    bh.push(5);
+    bh.push(8);
+    bh.push(4);
+    bh.push(2);
 
     bh.print();
 
-    std::cout << "\nmin: " << bh.getMin().first << endl;
+    std::cout << bh.top() << " ";
+    bh.pop();
+    std::cout << bh.top();
 
-    bh.extractMin();
-    bh.print();
-
-    BinomialHeap<int, int> bh2;
-    bh2.insert(-20, 5);
-    bh2.insert(-30, 6);
-
-    bh = bh + bh2;
-    cout << "\n";
-
-    bh.print();
-
-    bh.decrease(-20, -40);
-    bh.decrease(3, -2);
-    cout << "\n";
-
-    bh.print();
-
-    return 0;
 }
